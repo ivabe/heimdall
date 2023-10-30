526c957b714c5de434b6a441de33583cb11dbcb4
Iván A <ivan.abellan@uni.lu>
2023-10-30 23:03:13 +0100

    Generate proof, verify proofs of multiple attrs

--- heimdalljs/src/presentation/presentation.js
+++ heimdalljs/src/presentation/presentation.js
@@ -4,6 +4,7 @@ const snarkjs = require("snarkjs");
 const {performance} = require("perf_hooks");
 const {MAX_LEAF_SIZE} = require('../revocation');
 const {META_SIZE} = require('../credential');
+const { exit } = require("process");
 
 class Presentation {
     type
@@ -124,6 +125,49 @@ class Presentation {
         }
     }
 
+    async generateMult(nAttrs) {
+        if (nAttrs > 4) {
+            console.error("That many attributes not supported\n");
+            exit(1);
+        }
+        let root = path.join(require.main.paths[0].split("node_modules")[0].slice(0, -1), "../");
+        //let root = path.join(require.main.paths[0].split("node_modules")[0].slice(0, -1));
+        let t0 = performance.now();
+        const {proof, publicSignals} = await snarkjs.groth16.fullProve(
+            this.privateInput,
+            path.join(root, "zkp", "mult-attributes", "test.multipleAttributePresentation" +nAttrs+".wasm"),
+            path.join(root, "zkp", "mult-attributes", "test.multipleAttributePresentation"+nAttrs+".final.zkey")
+        );
+        let t1 = performance.now();
+
+        this.proof = proof;
+        this.publicSignals = publicSignals;
+
+        let res = await this.verifyMultProof(nAttrs);
+        // Overwriting private input
+        this.privateInput = {};
+
+        if (res === true) {
+            return Promise.resolve(true);
+        } else {
+            return Promise.resolve(false);
+        }
+    }
+
+    async verifyMultProof(nAttrs) {
+        // Multiple cases per number of attributes
+        let root = path.join(require.main.paths[0].split("node_modules")[0].slice(0, -1), "../");
+        //let root = path.join(require.main.paths[0].split("node_modules")[0].slice(0, -1));
+        const vKey = JSON.parse(fs.readFileSync(path.join(root, "zkp", "mult-attributes", "test.multipleAttributePresentation" +nAttrs+".verification.key.json")));
+
+        let res = await snarkjs.groth16.verify(vKey, this.publicSignals, this.proof).catch(err => console.error(err));
+        if (res === true) {
+            return Promise.resolve(true);
+        } else {
+            return Promise.reject(false);
+        }
+    }
+
     /**
      * Verifies the meta attributes from the proof
      * @param typeIndex {Number}
@@ -170,6 +214,78 @@ class Presentation {
                 '1696606287033'
             ]
          */
+        // Checks if meta type hash from public signal is the same like in public input
+        let res = hasher([this.output.meta.type]).toString() === this.publicSignals[typeIndex];
+        // Reads revocation root from public signal
+        this.output.meta.revocationRoot = this.publicSignals[revocationRootIndex];
+        // Where is the revocation root?
+        res &&= BigInt(this.revocationRoot) === BigInt(this.output.meta.revocationRoot)
+        // Checks if revocationRegistry of public input corresponds to hash of public signals
+        res &&= hasher([this.output.meta.revocationRegistry]).toString() ===
+            this.publicSignals[revocationRegistryHashIndex];
+        this.output.meta.revoked = Number(this.publicSignals[revokedIndex]) === 1;
+        this.output.meta.delegatable = Number(this.publicSignals[delegatableIndex]) === 1;
+        this.output.meta.linkBack = this.publicSignals[linkBackIndex];
+        this.output.meta.challenge = this.publicSignals[challengeIndex];
+        this.output.meta.expiration = this.publicSignals[expirationIndex];
+        if (typeof this.output.meta.issuerPK !== "undefined") {
+            res &&= hasher([
+                this.output.meta.challenge,
+                this.output.meta.issuerPK[0],
+                this.output.meta.issuerPK[1]
+            ]).toString() === this.output.meta.linkBack;
+        }
+        return Promise.resolve(res);
+    }
+
+      /**
+     * Verifies the meta attributes from the proof
+     * @param typeIndex {Number}
+     * @param revocationRootIndex {Number}
+     * @param revokedIndex {Number}
+     * @param revocationRegistryHashIndex
+     * @param linkBackIndex {Number}
+     * @param delegatableIndex {Number}
+     * @param challengeIndex {Number}
+     * @param expirationIndex {Number}
+     * @param hasher {Function}
+     * @returns {Promise<boolean>}
+     */
+      async verifyMultMeta(
+        typeIndex,
+        revocationRootIndex,
+        revocationRegistryHashIndex,
+        revokedIndex,
+        linkBackIndex,
+        delegatableIndex,
+        challengeIndex,
+        expirationIndex,
+        hasher
+    ) { 
+        /** Public Signals
+            type
+            revocationRoot  
+            revocationRegistry
+            revoked
+            linkBack  
+            delegatable
+            challenge
+            expiration
+            attributeHash[# attrs]
+         * [
+                '6936141895847827773039820306011898011976769516186037164536571405943971461449',
+                '15093063772197360439942670764347374738539884999170539844715519374005555450641',
+                '9037940188198198671970800601490910088551427182609940173326074139244911486789',
+                '0',
+                '16480984838845883908278887403998730505458370097797273028422755199897309800407',
+                '0',
+                '1234',
+                '1699108544456'
+                '506091454650568783913867607798865803589405944288788850564754505122530534451',
+                '3682517034118067363988451114871104117228742174037622396838237067437565515056',
+                ... (2) or more attributes if necessary
+            ]
+        */
         // Checks if meta type hash from public signal is the same like in public input
         let res = hasher([this.output.meta.type]).toString() === this.publicSignals[typeIndex];
         // Reads revocation root from public signal
